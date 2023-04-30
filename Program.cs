c9171a208b742c2c4983af888cfdaf9d1314cc01
mailz <sergey4istyak@yandex.ru>
2023-04-30 13:17:07 +0400

    прогоняем циклом for

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿
 int arraysize = int.Parse(Console.ReadLine()); 
 
+string[] array = new string[arraysize]; 
+
+for (int i = 0; i < arraysize; i++)
+{
+    Console.Write("Введите элементы массива: "); 
+    string result = Console.Readline();
+    array[i] = result; 
+}

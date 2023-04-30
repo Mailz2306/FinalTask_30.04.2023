31cdd2ea284f813798839af8b5136a1165b2cadd
mailz <sergey4istyak@yandex.ru>
2023-04-30 13:23:56 +0400

    создаём переменную count

--- Program.cs
+++ Program.cs
@@ -6,6 +6,22 @@ string[] array = new string[arraysize];
 for (int i = 0; i < arraysize; i++)
 {
     Console.Write("Введите элементы массива: "); 
-    string result = Console.Readline();
+    string result = Console.ReadLine();
     array[i] = result; 
+}
+
+Console.WriteLine();
+Console.Write("Это ваш массив: [");
+Console.Write(string.Join(",", array));
+Console.Write("]");
+
+int count = 0; 
+int maximumSimbol = 3; 
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= maximumSimbol)
+    {
+        count++; 
+    }
 }
